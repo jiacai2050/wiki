701997e40dccdcfc4b7a7dd71157f76a410e0879
jiacai2050 <jiacai2050@gmail.com>
2019-03-11 12:05:50 +0800

    updated at Mon Mar 11 12:05:50 CST 2019

--- site.sh
+++ site.sh
@@ -2,9 +2,11 @@
 
 set -xe
 
+REMOTE=${1:-oschina}
+
 gitbook build
 cp -r _book/* ../wiki-book
 cd ../wiki-book
 git add . -A
 git commit -m "updated at `date`"
-git push oschina gh-pages
+git push $REMOTE gh-pages
